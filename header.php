729fff953db6a86798c296f0cfbc4d353fcf293a
Christian Diomampo <chrisgen19@gmail.com>
2018-05-28 14:31:19 +0800

    Dynamic Navigation Menu

--- header.php
+++ header.php
@@ -26,48 +26,22 @@
 	
 	<header id="masthead" class="site-header">
 		<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 fixed-top bg-white border-bottom box-shadow">
-		<h5 class="my-0 mr-md-auto font-weight-normal"><img src="<?php echo get_template_directory_uri(); ?>/build/images/weext_logo-240x66.png" ></h5>
-		<nav class="my-2 my-md-0 mr-md-3">
-			<a class="p-2 text-dark" href="#">Home</a>
-			<a class="p-2 text-dark" href="#">Our Business</a>
-			<a class="p-2 text-dark" href="#">Partners</a>
-			<a class="p-2 text-dark" href="#">Our People</a>
-			<a class="p-2 text-dark" href="#">Work with us</a>
-			<a class="p-2 text-dark" href="#">Our Affiliates</a>
-			<a class="p-2 text-dark" href="#">Contact Us</a>
-		</nav>
+			<h5 class="my-0 mr-md-auto font-weight-normal">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" >
+					<img src="<?php echo get_template_directory_uri(); ?>/build/images/weext_logo-240x66.png" >
+				</a>
+			</h5>
+			<nav id="site-navigation"  class="main-navigation my-2 my-md-0 mr-md-3">
+
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+				
+			</nav><!-- #site-navigation -->
 		</div>
-	</header>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$weext_corp_description = get_bloginfo( 'description', 'display' );
-			if ( $weext_corp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $weext_corp_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'weext-corp' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
